2977aa6212b56a284f2503d7d35e7a99d152c67f
Yves Huynh <yhuynh@202-ecommerce.com>
2021-02-22 11:12:28 +0100

    feature #28043 - default configuration 'large default' for product feed

--- shoppingfeed.php
+++ shoppingfeed.php
@@ -378,6 +378,7 @@ class Shoppingfeed extends \ShoppingfeedClasslib\Module
         $this->setConfigurationDefault(self::ORDER_IMPORT_SHIPPED, false);
         $this->setConfigurationDefault(self::PRODUCT_FEED_CARRIER_REFERENCE, Configuration::getGlobalValue('PS_CARRIER_DEFAULT'));
         $this->setConfigurationDefault(self::ORDER_DEFAULT_CARRIER_REFERENCE, Configuration::getGlobalValue('PS_CARRIER_DEFAULT'));
+        $this->setConfigurationDefault(self::PRODUCT_FEED_IMAGE_FORMAT, 'large_default');
         $this->saveToken();
 
         return $res;
