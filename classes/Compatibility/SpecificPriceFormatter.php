36dc7529b478761ab550fe9396293175bedf0571
Bogdan Yampolskiy <byampolskiy@202-ecommerce.com>
2021-03-05 15:46:13 +0200

    refs #28371 compatibility with ps 1.6

--- classes/Compatibility/SpecificPriceFormatter.php
+++ classes/Compatibility/SpecificPriceFormatter.php
@@ -1,31 +1,23 @@
 <?php
 /**
- * 2007-2021 PayPal
+ * Copyright since 2019 Shopping Feed
  *
- *  NOTICE OF LICENSE
+ * NOTICE OF LICENSE
  *
- *  This source file is subject to the Academic Free License (AFL 3.0)
- *  that is bundled with this package in the file LICENSE.txt.
- *  It is also available through the world-wide-web at this URL:
- *  http://opensource.org/licenses/afl-3.0.php
- *  If you did not receive a copy of the license and are unable to
- *  obtain it through the world-wide-web, please send an email
- *  to license@prestashop.com so we can send you a copy immediately.
+ * This source file is subject to the Academic Free License (AFL 3.0)
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/AFL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to tech@202-ecommerce.com so we can send you a copy immediately.
  *
- *  DISCLAIMER
- *
- *  Do not edit or add to this file if you wish to upgrade PrestaShop to newer
- *  versions in the future. If you wish to customize PrestaShop for your
- *  needs please refer to http://www.prestashop.com for more information.
- *
- *  @author 2007-2021 PayPal
- *  @author 202 ecommerce <tech@202-ecommerce.com>
- *  @copyright PayPal
- *  @license http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @author    202 ecommerce <tech@202-ecommerce.com>
+ * @copyright Since 2019 Shopping Feed
+ * @license   https://opensource.org/licenses/AFL-3.0  Academic Free License (AFL 3.0)
  */
 
-use PrestaShop\PrestaShop\Adapter\Product\PriceFormatter;
-
+require_once _PS_MODULE_DIR_ . 'shoppingfeed/classes/Compatibility/PriceFormatter.php';
 
 class SpecificPriceFormatter
 {
