9954a4880eb50e6f821a2309f5446f7b32c5f0d9
Clotaire <52157233+clotairer@users.noreply.github.com>
2021-12-09 23:10:59 +0100

    add licence header and cast idLang and idShop

--- src/Services/ProductAttributeCombination.php
+++ src/Services/ProductAttributeCombination.php
@@ -1,5 +1,26 @@
 <?php
-
+/**
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to a commercial license from SARL 202 ecommence
+ * Use, copy, modification or distribution of this source file without written
+ * license agreement from the SARL 202 ecommence is strictly forbidden.
+ * In order to obtain a license, please contact us: tech@202-ecommerce.com
+ * ...........................................................................
+ * INFORMATION SUR LA LICENCE D'UTILISATION
+ *
+ * L'utilisation de ce fichier source est soumise a une licence commerciale
+ * concedee par la societe 202 ecommence
+ * Toute utilisation, reproduction, modification ou distribution du present
+ * fichier source sans contrat de licence ecrit de la part de la SARL 202 ecommence est
+ * expressement interdite.
+ * Pour obtenir une licence, veuillez contacter 202-ecommerce <tech@202-ecommerce.com>
+ * ...........................................................................
+ *
+ * @author    202-ecommerce <tech@202-ecommerce.com>
+ * @copyright Copyright (c) 202-ecommerce
+ * @license   Commercial license
+ */
 
 namespace ShoppingfeedAddon\Services;
 
@@ -56,10 +77,10 @@ class ProductAttributeCombination
             ->leftJoin('attribute_group', 'ag', 'ag.`id_attribute_group` = a.`id_attribute_group`')
             ->leftJoin('attribute_lang', 'al', 'a.`id_attribute` = al.`id_attribute`')
             ->leftJoin('attribute_group_lang', 'agl', 'ag.`id_attribute_group` = agl.`id_attribute_group`')
-            ->where('pas.id_shop = ' . $idShop)
-            ->where('al.id_lang = ' . $idLang)
-            ->where('agl.id_lang = ' . $idLang)
-            ->where('pa.id_product = ' . $product->id)
+            ->where('pas.id_shop = ' . (int) $idShop)
+            ->where('al.id_lang = ' . (int) $idLang)
+            ->where('agl.id_lang = ' . (int) $idLang)
+            ->where('pa.id_product = ' . (int) $product->id)
             ->groupBy('pa.`id_product_attribute`')
             ->orderBy('pa.`id_product_attribute`')
         ;
