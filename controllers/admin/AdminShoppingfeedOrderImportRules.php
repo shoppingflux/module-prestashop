aed59c067bc3c38432ee654a6ac7ec756028a6f3
Clotaire <52157233+clotairer@users.noreply.github.com>
2022-02-09 17:05:43 +0100

    Revert "refs #33202 upgrade 1.6.3"

--- controllers/admin/AdminShoppingfeedOrderImportRules.php
+++ controllers/admin/AdminShoppingfeedOrderImportRules.php
@@ -1,22 +1,20 @@
 <?php
 /**
+ * Copyright since 2019 Shopping Feed
  *
- *  Copyright since 2019 Shopping Feed
+ * NOTICE OF LICENSE
  *
- *  NOTICE OF LICENSE
+ * This source file is subject to the Academic Free License (AFL 3.0)
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/AFL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to tech@202-ecommerce.com so we can send you a copy immediately.
  *
- *  This source file is subject to the Academic Free License (AFL 3.0)
- *  that is bundled with this package in the file LICENSE.md.
- *  It is also available through the world-wide-web at this URL:
- *  https://opensource.org/licenses/AFL-3.0
- *  If you did not receive a copy of the license and are unable to
- *  obtain it through the world-wide-web, please send an email
- *  to tech@202-ecommerce.com so we can send you a copy immediately.
- *
- *  @author    202 ecommerce <tech@202-ecommerce.com>
- *  @copyright Since 2019 Shopping Feed
- *  @license   https://opensource.org/licenses/AFL-3.0  Academic Free License (AFL 3.0)
- *
+ * @author    202 ecommerce <tech@202-ecommerce.com>
+ * @copyright Since 2019 Shopping Feed
+ * @license   https://opensource.org/licenses/AFL-3.0  Academic Free License (AFL 3.0)
  */
 
 if (!defined('_PS_VERSION_')) {
@@ -542,7 +540,6 @@ class AdminShoppingfeedOrderImportRulesController extends ShoppingfeedAdminContr
         $order_sync_enabled = Tools::getValue(Shoppingfeed::ORDER_SYNC_ENABLED);
         $order_sync_test = Tools::getValue(Shoppingfeed::ORDER_IMPORT_TEST);
         $order_sync_shipped = Tools::getValue(Shoppingfeed::ORDER_IMPORT_SHIPPED);
-        $order_sync_shipped_marketplace = Tools::getValue(Shoppingfeed::ORDER_IMPORT_SHIPPED_MARKETPLACE);
 
         $shops = Shop::getShops();
         foreach ($shops as $shop) {
@@ -550,13 +547,6 @@ class AdminShoppingfeedOrderImportRulesController extends ShoppingfeedAdminContr
             Configuration::updateValue(Shoppingfeed::ORDER_IMPORT_TEST, ($order_sync_test ? true : false), false, null, $shop['id_shop']);
             Configuration::updateValue(Shoppingfeed::ORDER_SYNC_ENABLED, ($order_sync_enabled ? true : false), false, null, $shop['id_shop']);
             Configuration::updateValue(Shoppingfeed::ORDER_IMPORT_SHIPPED, ($order_sync_shipped ? true : false), false, null, $shop['id_shop']);
-            Configuration::updateValue(
-                Shoppingfeed::ORDER_IMPORT_SHIPPED_MARKETPLACE,
-                ($order_sync_shipped_marketplace && $order_sync_shipped? true : false),
-                false,
-                null,
-                $shop['id_shop']
-            );
         }
 
         $orderStatusesShipped = Tools::getValue('status_shipped_order');
