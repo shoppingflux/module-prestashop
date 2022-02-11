f1b79b2dbdab9df90fb948c52a7be164e54bb561
Clotaire <clotaire@202-ecommerce.com>
2022-02-11 15:00:00 +0100

    add github actions for unit test

--- 202/docker/entrypoint.sh
+++ 202/docker/entrypoint.sh
@@ -5,7 +5,13 @@ set -x
 
 /etc/init.d/mariadb start
 
-mysql -h localhost -u root -ppassword prestashop -e "UPDATE ps_shop_url SET domain='$PS_DOMAIN', domain_ssl='$PS_DOMAIN'"
+if [ "$PS_DOMAIN" ]; then 
+    mysql -h localhost -u root -ppassword prestashop -e "UPDATE ps_shop_url SET domain='$PS_DOMAIN', domain_ssl='$PS_DOMAIN'"
+fi
+
+cd  /var/www/html/modules/shoppingfeed
+
+composer update
 
 php /var/www/html/bin/console prestashop:module install shoppingfeed -e prod
 
@@ -14,11 +20,10 @@ INSERT IGNORE INTO ps_shoppingfeed_token (id_shoppingfeed_token, id_shop, id_lan
 (1, 1, 1, 1, 'token-shoppingfeed-api', 1, NOW(), NOW());
 "
 
-cd  /var/www/html/modules/shoppingfeed
-composer update 
-
-chown $RUN_USER:$RUN_USER /var/www/html -Rf
+if [ ${RUN_USER} ]; then
+    chown $RUN_USER:$RUN_USER /var/www/html -Rf
+else
+    chown www-data:www-data /var/www/html -Rf
+fi
 
 exec apache2-foreground
-
-
