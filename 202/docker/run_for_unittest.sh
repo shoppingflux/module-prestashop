4ff58e6be048969e2b66c2da374a928175562f5a
Clotaire <52157233+clotairer@users.noreply.github.com>
2022-02-16 09:12:59 +0100

    add docker configuration and phpunit on order import refs #33313 (#113)
    * add docker configuration and phpunit on order import refs #33313
    
    * add docker configuration and phpunit on order import refs #33313
    
    * fix github actions
    
    * fix github actions
    
    * add github actions for unit test
    
    * update README
    
    * add test KO to verify github actions
    
    * fix unitest
    
    * Add a new test on shopping feed
    
    * fix phpcs
    
    * add unit test for laredoute refs #31667
    
    * add unit test for laredoute refs #31667
    
    * add unit test for laredoute refs #31667
    
    * update doc and update docker config

---
+++ 202/docker/run_for_unittest.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+set -x
+
+/etc/init.d/mariadb start
+
+php /var/www/html/bin/console prestashop:module install shoppingfeed -e prod
+
+chown www-data:www-data /var/www/html/var -Rf
+
+mysql -h localhost -u root prestashop -e "
+INSERT IGNORE INTO ps_shoppingfeed_token (id_shoppingfeed_token, id_shop, id_lang, id_currency, content, active, date_add, date_upd) VALUES
+(1, 1, 1, 1, 'token-shoppingfeed-api', 1, NOW(), NOW());
+"
+cd /var/www/html/modules/shoppingfeed/
+
+vendor/bin/phpunit -c 202/phpunit.xml
---
+++ 202/tests/OrderImport/OrderImportSimpleTest.php
@@ -0,0 +1,152 @@
+<?php
+/**
+ * Copyright since 2019 Shopping Feed
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License (AFL 3.0)
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/AFL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to tech@202-ecommerce.com so we can send you a copy immediately.
+ *
+ * @author    202 ecommerce <tech@202-ecommerce.com>
+ * @copyright Since 2019 Shopping Feed
+ * @license   https://opensource.org/licenses/AFL-3.0  Academic Free License (AFL 3.0)
+ */
+
+namespace Tests\OrderImport;
+
+use ShoppingfeedClasslib\Actions\ActionsHandler;
+
+/**
+ * Order Rules AmazonEbay Test
+ */
+class OrderImportSimpleTest extends AbstractOrdeTestCase
+{
+    /**
+     * Test to import a standard order Amazon
+     *
+     * @return void
+     */
+    public function testImportAmazon(): void
+    {
+        $apiOrder = $this->getOrderRessourceFromDataset('order-amazon.json');
+
+        $handler = new ActionsHandler();
+        $handler->addActions(
+            'registerSpecificRules',
+            'verifyOrder',
+            'createOrderCart',
+            'validateOrder',
+            'acknowledgeOrder',
+            'recalculateOrderPrices',
+            'postProcess'
+        );
+
+        $handler->setConveyor(
+            [
+                'id_shop' => 1,
+                'id_token' => 1,
+                'apiOrder' => $apiOrder,
+            ]
+        );
+        $processResult = $handler->process('shoppingfeedOrderImport');
+
+        $this->assertTrue($processResult);
+
+        $conveyor = $handler->getConveyor();
+
+        $customer = $conveyor['customer'];
+        $this->assertEquals($customer->lastname, 'Martin');
+        $this->assertEquals($customer->firstname, 'Bernard');
+
+        $sfOrder = $conveyor['sfOrder'];
+        $this->assertEquals($sfOrder->name_marketplace, 'Amazon');
+        $this->assertEquals($sfOrder->id_order_marketplace, 'TEST-ORDER-AMAZON');
+
+        $psOrder = new \Order((int) $conveyor['id_order']);
+        // it's a test > canceled
+        $this->assertEquals($psOrder->current_state, _PS_OS_CANCELED_);
+        $this->assertEquals($psOrder->payment, 'amazon');
+        $this->assertEquals($psOrder->module, 'sfpayment');
+        $this->assertEquals($psOrder->total_discounts, 0.000000);
+        $this->assertEquals($psOrder->total_paid, 9.400000);
+        $this->assertEquals($psOrder->total_paid_tax_incl, 9.400000);
+        $this->assertEquals($psOrder->total_paid_tax_excl, 7.830000);
+        $this->assertEquals($psOrder->total_products, 3.750000);
+        $this->assertEquals($psOrder->total_shipping, 4.900000);
+    }
+
+    /**
+     * Test to import a standard order a second time the same
+     *
+     * @return void
+     */
+    public function testImportAmazonSecondTime(): void
+    {
+        $apiOrder = $this->getOrderRessourceFromDataset('order-amazon.json');
+
+        $handler = new ActionsHandler();
+        $handler->addActions(
+            'registerSpecificRules',
+            'verifyOrder',
+            'createOrderCart',
+            'validateOrder',
+            'acknowledgeOrder',
+            'recalculateOrderPrices',
+            'postProcess'
+        );
+
+        $handler->setConveyor(
+            [
+                'id_shop' => 1,
+                'id_token' => 1,
+                'apiOrder' => $apiOrder,
+            ]
+        );
+
+        $processResult = $handler->process('shoppingfeedOrderImport');
+
+        // expected not imported a second time !
+        $this->assertFalse($processResult);
+
+        $conveyor = $handler->getConveyor();
+        $this->assertEquals($conveyor['error'], 'Order not imported; already present.');
+    }
+
+    /**
+     * Test to import a standard order Nature & Decouverte
+     *
+     * @return void
+     */
+    public function testImportNatureDecouverte(): void
+    {
+        $apiOrder = $this->getOrderRessourceFromDataset('order-naturedecouverte.json');
+
+        $handler = new ActionsHandler();
+        $handler->addActions(
+            'registerSpecificRules',
+            'verifyOrder',
+            'createOrderCart',
+            'validateOrder',
+            'acknowledgeOrder',
+            'recalculateOrderPrices',
+            'postProcess'
+        );
+
+        $handler->setConveyor(
+            [
+                'id_shop' => 1,
+                'id_token' => 1,
+                'apiOrder' => $apiOrder,
+            ]
+        );
+
+        $processResult = $handler->process('shoppingfeedOrderImport');
+
+        $this->assertTrue($processResult);
+    }
+}
