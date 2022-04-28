163f110fbf251363c4fa56beb62f7e128d16a786
Clotaire <clotaire@202-ecommerce.com>
2022-04-28 13:34:47 +0200

    refs #34668 add unitest

--- 202/tests/OrderImport/OrderImportWithConfigTest.php
+++ 202/tests/OrderImport/OrderImportWithConfigTest.php
@@ -18,6 +18,8 @@
  */
 
 namespace Tests\OrderImport;
+use ShoppingfeedAddon\Actions\ActionsHandler;
+use ShoppingfeedClasslib\Registry;
 
 /**
  * Order Rules AmazonEbay Test
@@ -25,13 +27,65 @@ namespace Tests\OrderImport;
 class OrderImportWithConfigTest extends AbstractOrdeTestCase
 {
     /**
+     * Set Configurations
+     *
+     * @return void
+     */
+    public static function setUpBeforeClass(): void
+    {
+        \Configuration::updateGlobalValue(\Shoppingfeed::PRODUCT_FEED_REFERENCE_FORMAT, 'reference');
+    }
+
+    /**
      * Test to import a standard order Amazon
      *
      * @return void
      */
-    public function testImportAmazonAlreadyShipped(): void
+    public function testImportReferenceAsSKU(): void
     {
-        // Put here tests need different configuration before
-        $this->assertTrue(true);
+        $apiOrder = $this->getOrderRessourceFromDataset('order-veepee.json');
+
+        $handler = new ActionsHandler();
+        $handler->addActions(
+            'registerSpecificRules',
+            'verifyOrder',
+            'createOrderCart',
+            'validateOrder',
+            'postProcess'
+        );
+
+        $handler->setConveyor(
+            [
+                'id_shop' => 1,
+                'id_token' => 1,
+                'apiOrder' => $apiOrder,
+            ]
+        );
+        Registry::set('shoppingfeedOrderImportHandler', $handler);
+        $processResult = $handler->process('shoppingfeedOrderImport');
+
+        $this->assertTrue($processResult);
+        $conveyor = $handler->getConveyor();
+        $customer = $conveyor['customer'];
+        $this->assertEquals($customer->firstname, 'CAROLINE');
+        $this->assertEquals($customer->lastname, 'STEFANINI');
+        //$this->assertEquals($customer->email, 'caroline@domain.fr');
+
+        $sfOrder = $conveyor['sfOrder'];
+        $this->assertEquals($sfOrder->name_marketplace, 'Veepeegroup');
+        $this->assertEquals($sfOrder->id_order_marketplace, '200000VEEPEE200000');
+
+        $psOrder = $conveyor['psOrder'];
+        $this->assertEquals($psOrder->current_state, _PS_OS_PAYMENT_);
+    }
+
+    /**
+     * Unset Configurations
+     *
+     * @return void
+     */
+    public static function tearDownAfterClass(): void
+    {
+        \Configuration::updateGlobalValue(\Shoppingfeed::PRODUCT_FEED_REFERENCE_FORMAT, '');
     }
 }
