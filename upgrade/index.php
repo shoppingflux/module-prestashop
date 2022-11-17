23dc194d3fdf49de35bb7173ef9412037a82b074
Clotaire <clotaire@202-ecommerce.com>
2022-11-17 11:46:35 +0100

    update licences

--- upgrade/index.php
+++ upgrade/index.php
@@ -1,5 +1,5 @@
 <?php
-/*
+/**
 * 2007-2017 PrestaShop
 *
 * NOTICE OF LICENSE
@@ -20,11 +20,12 @@
 *
 *  @author PrestaShop SA <contact@prestashop.com>
 *  @copyright  2007-2016 PrestaShop SA
+*
 *  @version  Release: $Revision$
+*
 *  @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
 *  International Registered Trademark & Property of PrestaShop SA
 */
-
 header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
 
