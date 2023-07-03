934442246b60edf9dc75479f0eb25998b2f69e87
bogdan202 <byampolskiy@202-ecommerce.com>
2023-07-03 09:08:19 +0200

    refs #40990 small fix

--- controllers/admin/AdminShoppingfeedGeneralSettings.php
+++ controllers/admin/AdminShoppingfeedGeneralSettings.php
@@ -846,9 +846,9 @@ class AdminShoppingfeedGeneralSettingsController extends ShoppingfeedAdminContro
             ->from('category_lang', 'cl')
             ->leftJoin('category', 'c', 'cl.id_category = c.id_category')
             ->where('cl.id_lang = ' . (int) $this->context->language->id)
-            ->where('cl.id_shop = ' . (int) $this->context->shop->id)
             ->where('c.id_parent <> 0')
             ->orderBy('c.id_category ASC')
+            ->groupBy('cl.id_category')
             ->select('c.id_category as id, CONCAT("(", c.id_category, ")", " ", cl.name) as title');
 
         exit(json_encode(Db::getInstance()->executeS($query)));
