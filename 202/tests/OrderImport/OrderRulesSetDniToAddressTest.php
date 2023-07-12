f394e47e3949a0815fefb927726266364417b75a
bogdan202 <byampolskiy@202-ecommerce.com>
2023-07-12 14:02:29 +0200

    OrderRulesSetDniToAddressTest.php

---
+++ 202/tests/OrderImport/OrderRulesSetDniToAddressTest.php
@@ -0,0 +1,57 @@
+<?php
+/*
+ * 2007-2023 PayPal
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License (AFL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/afl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ *  versions in the future. If you wish to customize PrestaShop for your
+ *  needs please refer to http://www.prestashop.com for more information.
+ *
+ *  @author 2007-2023 PayPal
+ *  @author 202 ecommerce <tech@202-ecommerce.com>
+ *  @license http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ *  @copyright PayPal
+ *
+ */
+
+namespace Tests\OrderImport;
+
+use Address;
+use ShoppingfeedAddon\OrderImport\Rules\SetDniToAddress;
+
+class OrderRulesSetDniToAddressTest extends AbstractOrdeTestCase
+{
+    public function testSetDni()
+    {
+        $orderWithDni = $this->getOrderRessourceFromDataset('order-amazon.json');
+        $orderWithoutDni = $this->getOrderRessourceFromDataset('order-business.json');
+        $rule = new SetDniToAddress();
+
+        $this->assertTrue($rule->isApplicable($orderWithDni));
+        $params = [
+            'billingAddress' => new Address(),
+            'apiOrder' => $orderWithDni,
+        ];
+        $rule->beforeBillingAddressSave($params);
+        $this->assertNotEmpty($params['billingAddress']->dni);
+
+        $this->assertFalse($rule->isApplicable($orderWithoutDni));
+        $params = [
+            'billingAddress' => new Address(),
+            'apiOrder' => $orderWithoutDni,
+        ];
+        $rule->beforeBillingAddressSave($params);
+        $this->assertEmpty($params['billingAddress']->dni);
+    }
+}
