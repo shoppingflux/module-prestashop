49ab46937057c098074074a7666dc9add71eafa9
jeremie <jeremie@studio-kiwik.fr>
2023-09-05 16:37:28 +0200

    Compatibility for PHP 5.6
    Use full namespace for SfTools, avoiding module install error on PHP 5.6

--- shoppingfeed.php
+++ shoppingfeed.php
@@ -17,8 +17,6 @@
  * @license   https://opensource.org/licenses/AFL-3.0  Academic Free License (AFL 3.0)
  */
 
-use ShoppingfeedAddon\Services\SfTools;
-
 if (!defined('_PS_VERSION_')) {
     exit;
 }
@@ -37,6 +35,7 @@ require_once _PS_MODULE_DIR_ . 'shoppingfeed/vendor/autoload.php';
 // use ShoppingfeedAddon\Hook\HookDispatcher;
 // use ShoppingfeedAddon\ProductFilter\FilterFactory;
 // use ShoppingfeedAddon\Services\OrderTracker;
+// use ShoppingfeedAddon\Services\SfTools;
 
 /**
  * The base module class
@@ -307,7 +306,7 @@ class Shoppingfeed extends \ShoppingfeedClasslib\Module
         $this->ps_versions_compliancy = ['min' => '1.6', 'max' => '8.99.99'];
         $this->need_instance = false;
         $this->bootstrap = true;
-        $this->tools = new SfTools();
+        $this->tools = new \ShoppingfeedAddon\Services\SfTools();
 
         parent::__construct();
 
