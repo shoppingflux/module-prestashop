a8f86d6fa82758f305ba90f2e6d9ed6bc11998dc
Clotaire <52157233+clotairer@users.noreply.github.com>
2023-11-27 11:45:26 +0100

    Release 1.10.5 (#313)
    * Address issue #307: Undefined index: env in OrderImport  (#309)
    
    * Address issue #307: Undefined index:
    
    Test array key isset before trying to access it.
    
    * php-cs-fixer fix
    
    * Compatibility with php 7.2.x and lower (43122) #311
    
    * Update build.xml
    
    ---------
    
    Co-authored-by: Léa BM Services <123975281+lea-BMS@users.noreply.github.com>
    Co-authored-by: bogdan202 <44570209+bogdan202@users.noreply.github.com>

--- src/OrderImport/Rules/ShippedByMarketplace.php
+++ src/OrderImport/Rules/ShippedByMarketplace.php
@@ -256,7 +256,11 @@ class ShippedByMarketplace extends RuleAbstract implements RuleInterface
     protected function isShippedAmazon(OrderResource $apiOrder)
     {
         try {
-            return strpos(strtolower($apiOrder->getPaymentInformation()['method']), 'afn') !== false;
+            $paymentInformation = $apiOrder->getPaymentInformation();
+
+            return
+                isset($paymentInformation['method']) &&
+                strpos(strtolower($paymentInformation['method']), 'afn') !== false;
         } catch (\Throwable $e) {
             return false;
         }
@@ -270,7 +274,11 @@ class ShippedByMarketplace extends RuleAbstract implements RuleInterface
     protected function isShippedCdiscount(OrderResource $apiOrder)
     {
         try {
-            return strpos(strtolower($apiOrder->getPaymentInformation()['method']), 'clogistique') !== false;
+            $paymentOrderInformation = $apiOrder->getPaymentInformation();
+
+            return
+                isset($paymentOrderInformation['method']) &&
+                strpos(strtolower($paymentOrderInformation['method']), 'clogistique') !== false;
         } catch (\Throwable $e) {
             return false;
         }
@@ -284,7 +292,11 @@ class ShippedByMarketplace extends RuleAbstract implements RuleInterface
     protected function isShippedManomano(OrderResource $apiOrder)
     {
         try {
-            return strtolower($apiOrder->toArray()['additionalFields']['env']) == 'epmm';
+            $apiOrderArray = $apiOrder->toArray();
+
+            return
+                isset($apiOrderArray['additionalFields']['env']) &&
+                strtolower($apiOrderArray['additionalFields']['env']) == 'epmm';
         } catch (\Throwable $e) {
             return false;
         }
