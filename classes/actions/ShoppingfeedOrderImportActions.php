7e3af8ea87bc4ea2135b00a49c1446e446e2b273
bogdan202 <44570209+bogdan202@users.noreply.github.com>
2025-03-17 12:30:46 +0100

    refs #49022 update shoppingfeed php-sdk (#417)
    * refs #49022 update shoppingfeed php-sdk
    
    * refs #51116 acknowledge action
    
    * refs #51116 rename a variable

--- classes/actions/ShoppingfeedOrderImportActions.php
+++ classes/actions/ShoppingfeedOrderImportActions.php
@@ -882,7 +882,7 @@ class ShoppingfeedOrderImportActions extends DefaultActions
                 $apiOrder->getId(),
                 isset($this->conveyor['id_order']) ? $this->conveyor['id_order'] : null,
                 $isSucess,
-                empty($this->conveyor['error']) ? null : $this->conveyor['error'],
+                empty($this->conveyor['error']) ? '' : $this->conveyor['error'],
                 $this->conveyor['shoppingfeed_store_id']
             );
         } catch (Exception $e) {
