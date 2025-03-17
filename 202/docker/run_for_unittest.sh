366cbf349966072ec7c598ff55cfca155682cc6b
Clotaire <52157233+clotairer@users.noreply.github.com>
2025-03-17 14:10:14 +0100

    Prevent Mysql Gone Away during unit test launch on docker container (#393)
    * update run_for_unittest script
    
    * update run_for_unittest script
    
    * update run_for_unittest script
    
    * update run_for_unittest script
    
    * update run_for_unittest script
    
    * update run_for_unittest script
    
    * update run_for_unittest script
    
    * update run_for_unittest script
    
    * Update run_for_unittest.sh

--- 202/docker/run_for_unittest.sh
+++ 202/docker/run_for_unittest.sh
@@ -5,16 +5,22 @@ set -x
 
 /etc/init.d/mariadb start
 
-php /var/www/html/bin/console prestashop:module install shoppingfeed
-php /var/www/html/bin/console prestashop:module install dpdfrance
-php /var/www/html/bin/console prestashop:module install colissimo 
-php /var/www/html/bin/console prestashop:module install mondialrelay 
-php /var/www/html/bin/console prestashop:module install nkmgls 
+sleep 3
+mysql -h localhost -u root prestashop -e "
+SET GLOBAL wait_timeout=6000;
+SET GLOBAL interactive_timeout=6000;
+SET GLOBAL max_allowed_packet=1073741824;
+"
+
+php /var/www/html/bin/console prestashop:module install shoppingfeed -e prod
+php /var/www/html/bin/console prestashop:module install dpdfrance -e prod
+php /var/www/html/bin/console prestashop:module install colissimo -e prod
+php /var/www/html/bin/console prestashop:module install mondialrelay -e prod
+php /var/www/html/bin/console prestashop:module install nkmgls -e prod
 
 echo "Add data fixtures for Unit Tests"
 
 mysql -h localhost -u root prestashop -e "
-
 TRUNCATE ps_shoppingfeed_preloading;
 TRUNCATE ps_mondialrelay_carrier_method;
 INSERT INTO ps_mondialrelay_carrier_method (id_carrier, delivery_mode, insurance_level, is_deleted, id_reference, date_add, date_upd)
@@ -31,7 +37,9 @@ INSERT INTO ps_configuration (id_shop_group, id_shop, name, value, date_add, dat
 
 UPDATE ps_product_attribute SET reference = 'demo_17_white' WHERE id_product = 11 AND default_on = 1;
 UPDATE ps_product_attribute_shop SET ecotax = 6 WHERE id_product = 5 AND id_product_attribute = 19;;
+"
 
+mysql -h localhost -u root prestashop -e "
 TRUNCATE ps_shoppingfeed_order;
 TRUNCATE ps_shoppingfeed_task_order;
 UPDATE ps_configuration SET value = '[\"5\",\"4\"]' WHERE name = 'SHOPPINGFEED_SHIPPED_ORDERS';
@@ -57,7 +65,8 @@ UPDATE ps_product_shop SET id_tax_rules_group = 10 WHERE id_product = 18;
 
 INSERT IGNORE INTO ps_shoppingfeed_token (id_shoppingfeed_token, id_shop, id_lang, id_currency, content, active, date_add, date_upd) VALUES
 (1, 1, 1, 1, 'token-shoppingfeed-api', 1, NOW(), NOW());
-
+"
+mysql -h localhost -u root prestashop -e "
 REPLACE INTO ps_shoppingfeed_carrier (id_shoppingfeed_carrier, name_marketplace, name_carrier, id_carrier_reference, is_new, date_add, date_upd) VALUES
 (1,	'Amazon',	'Colissimo',	1,	0,	'2022-02-17 00:00:00',	'2022-02-17 00:00:00'),
 (2,	'NatureEtDecouvertes',	'Point Relais (Chrono Relais)',	2,	0,	'2022-02-17 00:00:00',	'2022-02-17 00:00:00'),
@@ -87,7 +96,8 @@ REPLACE INTO ps_cart_rule (id_cart_rule ,id_customer,date_from,date_to,descripti
     VALUES (1, 0,'2022-02-17 00:00:00','2042-02-17 00:00:00','',999,999,1,1,'',0.000000,0,1,0,0,0,0,0,0,0,0,0.00,0.000000,0,1,0,0,7,0,0,1,'2022-02-17 00:00:00','2022-02-17 00:00:00');
 REPLACE INTO ps_cart_rule_lang (id_cart_rule,id_lang,name)
     VALUES (1,1,'gift');
-
+"
+mysql -h localhost -u root prestashop -e "
 TRUNCATE ps_colissimo_pickup_point;
 
 INSERT IGNORE INTO ps_feature (id_feature,  position)
