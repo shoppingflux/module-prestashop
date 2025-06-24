1fc3cecedb42c1603c99886eda4dcb12fa86d387
bogdan202 <byampolskiy@202-ecommerce.com>
2025-06-24 14:47:16 +0200

    refs #52845 nkmgls compatibility

--- src/OrderImport/GLS/CartCarrierAssociation.php
+++ src/OrderImport/GLS/CartCarrierAssociation.php
@@ -31,11 +31,14 @@ class CartCarrierAssociation
     protected $db;
 
     protected $glsAdapter;
+    /** @var \Module */
+    protected $nkmgls = null;
 
     public function __construct(AdapterInterface $glsAdapter)
     {
         $this->db = Db::getInstance();
         $this->glsAdapter = $glsAdapter;
+        $this->nkmgls = \Module::getInstanceByName('nkmgls');
     }
 
     public function create(Cart $cart, $relayId = null)
@@ -58,10 +61,13 @@ class CartCarrierAssociation
         );
 
         $this->db->delete('gls_cart_carrier', 'id_cart = "' . pSQL($cart->id) . '"');
-        $sql = 'INSERT IGNORE INTO ' . _DB_PREFIX_ . "gls_cart_carrier VALUES (
+
+        if (version_compare($this->nkmgls->version, '3.2.0', '>=')) {
+            $sql = 'INSERT IGNORE INTO ' . _DB_PREFIX_ . "gls_cart_carrier VALUES (
                 '" . (int) $cart->id . "',
                 '" . (int) $cart->id_customer . "',
-                '" . (int) Configuration::get('GLS_GLSRELAIS_ID', (int) $cart->id_carrier, $cart->id_shop_group, $cart->id_shop) . "',
+                '" . (int) \Configuration::get('GLS_GLSRELAIS_ID', (int) $cart->id_carrier, $cart->id_shop_group, $cart->id_shop) . "',
+                '" . pSQL($address->id) . "',
                 '" . pSQL($gls_product) . "',
                 '" . (empty($relayId) ? '' : pSQL($relayId)) . "',
                 '" . (empty($relay_detail['Name1']) ? '' : pSQL($relay_detail['Name1'])) . "',
@@ -75,6 +81,25 @@ class CartCarrierAssociation
                 '" . (int) $country->id . "',
                 '" . (empty($relay_detail['GLSWorkingDay']) ? '' : pSQL(json_encode($relay_detail['GLSWorkingDay']))) . "'
             )";
+        } else {
+            $sql = 'INSERT IGNORE INTO ' . _DB_PREFIX_ . "gls_cart_carrier VALUES (
+                '" . (int) $cart->id . "',
+                '" . (int) $cart->id_customer . "',
+                '" . (int) \Configuration::get('GLS_GLSRELAIS_ID', (int) $cart->id_carrier, $cart->id_shop_group, $cart->id_shop) . "',
+                '" . pSQL($gls_product) . "',
+                '" . (empty($relayId) ? '' : pSQL($relayId)) . "',
+                '" . (empty($relay_detail['Name1']) ? '' : pSQL($relay_detail['Name1'])) . "',
+                '" . (empty($relay_detail['Street1']) ? '' : pSQL($relay_detail['Street1'])) . "',
+                '" . (empty($relay_detail['Street2']) ? '' : pSQL($relay_detail['Street2'])) . "',
+                '" . (empty($relay_detail['ZipCode']) ? '' : pSQL($relay_detail['ZipCode'])) . "',
+                '" . (empty($relay_detail['City']) ? '' : pSQL($relay_detail['City'])) . "',
+                '" . (empty($relay_detail['Phone']) ? '' : pSQL($relay_detail['Phone'])) . "',
+                '" . (empty($relay_detail['Mobile']) ? '' : pSQL($relay_detail['Mobile'])) . "',
+                '" . pSQL($address->phone_mobile) . "',
+                '" . (int) $country->id . "',
+                '" . (empty($relay_detail['GLSWorkingDay']) ? '' : pSQL(json_encode($relay_detail['GLSWorkingDay']))) . "'
+            )";
+        }
 
         return $this->db->execute($sql);
     }
