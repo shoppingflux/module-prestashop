b2d7906707486b791c4d2e4b1bc3eada4e4f133a
bogdan202 <byampolskiy@202-ecommerce.com>
2025-10-23 10:43:54 +0200

    ps validator

--- classes/actions/ShoppingfeedOrderImportActions.php
+++ classes/actions/ShoppingfeedOrderImportActions.php
@@ -577,11 +577,7 @@ class ShoppingfeedOrderImportActions extends DefaultActions
 
         $cart->id_carrier = $this->conveyor['carrier']->id;
         $cart->id_shop = $this->getIdShop();
-        if (version_compare(_PS_VERSION_, '1.7.2.5', '>=')) {
-            $cart->delivery_option = json_encode([$cart->id_address_delivery => $cart->id_carrier . ',']);
-        } else {
-            $cart->delivery_option = @serialize([$cart->id_address_delivery => $cart->id_carrier . ',']);
-        }
+        $cart->delivery_option = json_encode([$cart->id_address_delivery => $cart->id_carrier . ',']);
         $cart->add();
 
         ProcessLoggerHandler::logInfo(
--- shoppingfeed.php
+++ shoppingfeed.php
@@ -335,7 +335,7 @@ class Shoppingfeed extends ShoppingfeedClasslib\Module
         $this->version = '@version@';
         $this->author = '202 ecommerce';
         $this->tab = 'market_place';
-        $this->ps_versions_compliancy = ['min' => '1.6', 'max' => '9.99.99'];
+        $this->ps_versions_compliancy = ['min' => '1.7.6', 'max' => '9.99.99'];
         $this->need_instance = 0;
         $this->bootstrap = true;
         $this->module_key = '7251acf8971b8f1de58ce48f01f86a9d';
