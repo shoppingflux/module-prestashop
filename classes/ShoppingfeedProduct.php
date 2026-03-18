67d6445593c4630eabbd47c541f2188fee6e4dc5
bogdan202 <byampolskiy@202-ecommerce.com>
2026-03-18 15:31:33 +0100

    refs #57196 product ean13

--- classes/ShoppingfeedProduct.php
+++ classes/ShoppingfeedProduct.php
@@ -142,6 +142,10 @@ class ShoppingfeedProduct extends ObjectModel
         $sql->where($where);
         $reference = Db::getInstance(_PS_USE_SQL_SLAVE_)->getValue($sql);
 
+        if (empty($reference) && empty($this->id_product_attribute) && $reference_format === 'ean13') {
+            $reference = (string) $this->id_product;
+        }
+
         return is_string($reference) ? trim($reference) : '';
     }
 
